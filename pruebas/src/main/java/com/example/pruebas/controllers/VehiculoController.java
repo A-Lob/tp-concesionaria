edbb271eac9ffc89b92100dc9765dadb1625ba70
franco lujan <francolujann1@gmail.com>
2024-11-04 23:03:45 -0300

    mas apis, mejoras en modelos DTOS

--- pruebas/src/main/java/com/example/pruebas/controllers/VehiculoController.java
+++ pruebas/src/main/java/com/example/pruebas/controllers/VehiculoController.java
@@ -1,23 +1,13 @@
 package com.example.pruebas.controllers;
 
-import com.example.pruebas.dtos.ModeloDTO;
-import com.example.pruebas.dtos.PosicionDTO;
-import com.example.pruebas.dtos.PruebaDTO;
-import com.example.pruebas.dtos.VehiculoDTO;
 import com.example.pruebas.dtos.detallesDto.DetalleVehiculoDTO;
-import com.example.pruebas.models.Vehiculo;
 import com.example.pruebas.services.implementations.VehiculoServiceImpl;
-import com.example.pruebas.services.interfaces.VehiculoService;
-import jakarta.servlet.ServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -31,11 +21,19 @@ public class VehiculoController {
 
     }
 
-
-    @GetMapping("/todos")
-    public ResponseEntity<List<DetalleVehiculoDTO>> getVehiculoId(ServletRequest servletRequest) {
+    @GetMapping("buscar/{id}")
+    public ResponseEntity<DetalleVehiculoDTO> buscarVehiculo(@PathVariable int id) {
         try{
-            List<DetalleVehiculoDTO> lista = vehiculoService.findAllD();
+            DetalleVehiculoDTO  vehiculo = vehiculoService.obtenerDetalleVehiculo(id);
+            return ResponseEntity.ok().body(vehiculo);
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    @GetMapping("/todos")
+    public ResponseEntity<List<DetalleVehiculoDTO>> getVehiculo() {
+        try{
+            List<DetalleVehiculoDTO> lista = vehiculoService.obtenerDetallesVehiculos();
             return ResponseEntity.ok().body(lista);
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
--- pruebas/src/main/java/com/example/pruebas/services/implementations/VehiculoServiceImpl.java
+++ pruebas/src/main/java/com/example/pruebas/services/implementations/VehiculoServiceImpl.java
@@ -5,11 +5,9 @@ import com.example.pruebas.dtos.PosicionDTO;
 import com.example.pruebas.dtos.PruebaDTO;
 import com.example.pruebas.dtos.VehiculoDTO;
 import com.example.pruebas.dtos.detallesDto.DetalleVehiculoDTO;
-import com.example.pruebas.models.Modelo;
 import com.example.pruebas.models.Posicion;
 import com.example.pruebas.models.Prueba;
 import com.example.pruebas.models.Vehiculo;
-import com.example.pruebas.repositories.ModeloRepository;
 import com.example.pruebas.repositories.PosicionRepository;
 import com.example.pruebas.repositories.PruebaRepository;
 import com.example.pruebas.repositories.VehiculoRepository;
@@ -17,6 +15,7 @@ import com.example.pruebas.services.interfaces.VehiculoService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehiculoServiceImpl extends ServiceImpl<Vehiculo, Integer> implements VehiculoService {
@@ -24,13 +23,13 @@ public class VehiculoServiceImpl extends ServiceImpl<Vehiculo, Integer> implemen
     private final VehiculoRepository vehiculoRepository;
     private final PruebaRepository pruebaRepository;
     private final PosicionRepository posicionRepository;
-    private final ModeloRepository modeloRepository;
 
-    public VehiculoServiceImpl(VehiculoRepository vehiculoRepository, PruebaRepository pruebaRepository, PosicionRepository posicionRepository, ModeloRepository modeloRepository) {
+
+    public VehiculoServiceImpl(VehiculoRepository vehiculoRepository, PruebaRepository pruebaRepository, PosicionRepository posicionRepository) {
         this.vehiculoRepository = vehiculoRepository;
         this.pruebaRepository = pruebaRepository;
         this.posicionRepository = posicionRepository;
-        this.modeloRepository = modeloRepository;
+
     }
 
     @Override
@@ -38,7 +37,6 @@ public class VehiculoServiceImpl extends ServiceImpl<Vehiculo, Integer> implemen
         this.vehiculoRepository.save(vehiculo);
     }
 
-    @Override
     public void update(Vehiculo vehiculo) {
         this.vehiculoRepository.save(vehiculo);
     }
@@ -54,18 +52,97 @@ public class VehiculoServiceImpl extends ServiceImpl<Vehiculo, Integer> implemen
         return this.vehiculoRepository.findById(id).orElseThrow();
     }
 
-    public List<DetalleVehiculoDTO> findAllD() {
+    @Override
+
+    public List<Vehiculo> findAll() {
+        return this.vehiculoRepository.findAll();
+    }
+
+    public List<DetalleVehiculoDTO> obtenerDetallesVehiculos() {
         List<Vehiculo> vehiculos = vehiculoRepository.findAll();
         List<Prueba> pruebas = pruebaRepository.findAll();
-        List<PruebaDTO> pruebasDtos = pruebas.stream().map(p -> {
-            PruebaDTO dto = new PruebaDTO();
-            dto.setIdInteresado(p.getInteresado().getId());
-            dto.setIdVehiculo(p.getVehiculo().getId());
-            dto.setLegajoEmpleado(p.getEmpleado().getLegajo());
-            return dto;
-        }).toList();
+
 
         List<Posicion> posicion = posicionRepository.findAll();
+
+
+        List<DetalleVehiculoDTO> detalleVehiculoDTO = vehiculos.stream().map(e -> {
+                    DetalleVehiculoDTO dto = new DetalleVehiculoDTO();
+                    VehiculoDTO vehiculoDTO = new VehiculoDTO();
+                    List<PruebaDTO> pruebasDtos = this.listadoPruebasDto(pruebas, List.of(e));
+                    List<PosicionDTO> posicionDtos = this.listadoPosicionesDto(posicion, List.of(e));
+
+                    vehiculoDTO.setPatente(e.getPatente());
+                    vehiculoDTO.setAnio(e.getAnio());
+
+                    dto.setVehiculo(vehiculoDTO);
+                    dto.setPruebas(pruebasDtos);
+                    dto.setPosiciones(posicionDtos);
+                    ModeloDTO modelo = new ModeloDTO(e.getModelo().getDescripcion());
+                    dto.setModelo(modelo);
+                    return dto;
+                })
+                .toList();
+
+
+        return detalleVehiculoDTO;
+    }
+
+    public DetalleVehiculoDTO obtenerDetalleVehiculo(Integer id) {
+        Vehiculo vehiculo = vehiculoRepository.findById(id).orElseThrow();
+        DetalleVehiculoDTO dtoVehiculo = new DetalleVehiculoDTO();
+        List<Prueba> pruebas = pruebaRepository.findAll();
+        List<PruebaDTO> pruebasDtos = this.listadoPruebasDto(pruebas, id);
+        List<Posicion> posicion = posicionRepository.findAll();
+        List<PosicionDTO> posicionDtos = this.listadoPosicionesDto(posicion, id);
+
+
+        VehiculoDTO vehiculoDTO = new VehiculoDTO();
+        vehiculoDTO.setPatente(vehiculo.getPatente());
+        vehiculoDTO.setAnio(vehiculo.getAnio());
+
+        dtoVehiculo.setPruebas(pruebasDtos);
+        dtoVehiculo.setPosiciones(posicionDtos);
+        dtoVehiculo.setVehiculo(vehiculoDTO);
+        ModeloDTO modelo = new ModeloDTO(vehiculo.getModelo().getDescripcion());
+        dtoVehiculo.setModelo(modelo);
+        return dtoVehiculo;
+    }
+
+    private List<PruebaDTO> listadoPruebasDto(List<Prueba> pruebas, List<Vehiculo> vehiculos) {
+        List<PruebaDTO> pruebasDtos = pruebas.stream()
+                .map(p -> {
+                    PruebaDTO dto = new PruebaDTO();
+                    dto.setIdInteresado(p.getInteresado().getId());
+                    dto.setIdVehiculo(p.getVehiculo().getId());
+                    dto.setLegajoEmpleado(p.getEmpleado().getLegajo());
+                    return dto;
+                }).toList();
+        List<PruebaDTO> pruebasDtosF = pruebasDtos.stream()
+                .filter(p1 -> vehiculos.stream().anyMatch(p2 -> p2.getId() == p1.getIdVehiculo()))
+                .toList();
+
+
+        return pruebasDtosF;
+    }
+
+    private List<PruebaDTO> listadoPruebasDto(List<Prueba> pruebas, int id) {
+        List<PruebaDTO> pruebasDtos = pruebas.stream()
+                .map(p -> {
+                    PruebaDTO dto = new PruebaDTO();
+                    dto.setIdInteresado(p.getInteresado().getId());
+                    dto.setIdVehiculo(p.getVehiculo().getId());
+                    dto.setLegajoEmpleado(p.getEmpleado().getLegajo());
+                    return dto;
+                }).toList();
+
+        pruebasDtos = pruebasDtos.stream().filter(p -> p.getIdVehiculo() == id).toList();
+
+        return pruebasDtos;
+    }
+
+    private List<PosicionDTO> listadoPosicionesDto(List<Posicion> posicion, List<Vehiculo> vehiculos) {
+
         List<PosicionDTO> posicionDtos = posicion.stream().map(pos -> {
             PosicionDTO dto = new PosicionDTO();
             dto.setIdVehiculo(pos.getVehiculo().getId());
@@ -74,30 +151,22 @@ public class VehiculoServiceImpl extends ServiceImpl<Vehiculo, Integer> implemen
             return dto;
         }).toList();
 
-       List<DetalleVehiculoDTO> detalleVehiculoDTO = vehiculos.stream().map(e->{
-            DetalleVehiculoDTO dto = new DetalleVehiculoDTO();
-            VehiculoDTO vehiculoDTO = new VehiculoDTO();
-
-            vehiculoDTO.setPatente(e.getPatente());
-            vehiculoDTO.setAnio(e.getAnio());
-
-            dto.setVehiculo(vehiculoDTO);
-            dto.setPruebas(pruebasDtos);
-            dto.setPosiciones(posicionDtos);
-           ModeloDTO modelo = new ModeloDTO(e.getModelo().getDescripcion());
-           dto.setModelo(modelo);
-           return dto;
-                })
-                .toList();
-
-
-
-        return detalleVehiculoDTO;
-    }
-    @Override
-
-    public List<Vehiculo> findAll(){
-        return this.vehiculoRepository.findAll();
+        List<PosicionDTO> posicionDtosF = posicionDtos.stream()
+                .filter(p1 -> vehiculos.stream().anyMatch(p2 -> p2.getId() == p1.getIdVehiculo()))
+                .toList();;
+        return posicionDtosF;
     }
 
+    private List<PosicionDTO> listadoPosicionesDto(List<Posicion> posicion, int id) {
+
+        List<PosicionDTO> posicionDtos = posicion.stream().map(pos -> {
+            PosicionDTO dto = new PosicionDTO();
+            dto.setIdVehiculo(pos.getVehiculo().getId());
+            dto.setLatitud(pos.getLatitud());
+            dto.setLongitud(pos.getLongitud());
+            return dto;
+        }).toList();
+        posicionDtos = posicionDtos.stream().filter(e -> e.getIdVehiculo() == id).toList();
+        return posicionDtos;
+    }
 }
