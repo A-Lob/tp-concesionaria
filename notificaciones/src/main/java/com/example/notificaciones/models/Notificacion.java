a3ead9be26a5315232fb7303067434064ff8b115
A-Lob <alex.lobaiza3@gmail.com>
2024-11-07 19:28:48 -0300

    Changes in Notification Validations

--- notificaciones/src/main/java/com/example/notificaciones/models/Notificacion.java
+++ notificaciones/src/main/java/com/example/notificaciones/models/Notificacion.java
@@ -1,10 +1,15 @@
 package com.example.notificaciones.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Notificaciones")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Notificacion {
 
@@ -13,12 +18,19 @@ public class Notificacion {
     private int id;
 
     // Destinatario de la notificacion
+    @Column(name="MAIL", nullable = false)
+    @NotBlank(message = "Campo Mail vacío")
+    @Email(message = "Formato de dirección e-mail incorrecto")
     private String email;
 
     // Razon del envio de la notificacion
-    private String asunto;
+    @Column(name="ASUNTO", nullable = false, length = 20)
+    @Size(max = 20, message = "El asunto no puede exceder los 20 caracteres")
+    @NotBlank(message = "El asunto no debe estar vacío")
+    private String asunto = "Fuera de Radio";
 
     // Lo que se envia en cada notificacion
-    private String contenido;
-
+    @Column(name="DESCRIPCION", length = 300)
+    @Size(max = 300)
+    private String descripcion;
 }
