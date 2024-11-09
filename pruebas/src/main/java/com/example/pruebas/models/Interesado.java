0231c0b17b2344c32fee69352c6e66bdf6a7a80d
A-Lob <alex.lobaiza3@gmail.com>
2024-11-09 20:24:25 -0300

    Tiny Changes

--- pruebas/src/main/java/com/example/pruebas/models/Interesado.java
+++ pruebas/src/main/java/com/example/pruebas/models/Interesado.java
@@ -56,7 +56,7 @@ public class Interesado {
     @JsonManagedReference
     private List<Prueba> pruebas;
 
-    @Column(name = "Estado", nullable = false)
+    @Column(name = "ESTADO", nullable = false)
     private String estado = "Habilitado";
 
 }
